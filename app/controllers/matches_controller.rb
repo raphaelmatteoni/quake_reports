1ffbcfbdf7f8231feb742fc63955fadf7438be40
Raphael Matteoni <raphaelmatteoni@gmail.com>
2024-02-05 00:47:32 -0300

    add death cause html

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -16,6 +16,7 @@ class MatchesController < ApplicationController
     @matches = Match.includes(:kills).all
 
     respond_to do |format|
+      format.html
       format.json
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link(text, path)
+    class_name = current_page?(path) ? 'active' : ''
+
+    content_tag(:div, class: 'menu') do
+      link_to(text, path, class: class_name)
+    end
+  end
 end
