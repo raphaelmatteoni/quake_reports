def6632674c48874bb56f2baca471711b3594681
Raphael Matteoni <raphaelmatteoni@gmail.com>
2024-02-05 15:36:30 -0300

    add top players and top causes at pages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class MatchesController < ApplicationController
-  before_action :setup_top_players, only: %i[general_report]
+  before_action :setup_top_killers, only: %i[general_report]
+  before_action :setup_top_causes, only: %i[death_cause_report]
 
   def index ; end
 
   def general_report
-    @matches = Match.includes(:kills, :match_players).all
+    @matches = Match.includes(:kills, :match_players, match_players: [:player]).all
 
     respond_to do |format|
       format.html
@@ -25,10 +26,18 @@ class MatchesController < ApplicationController
 
   private
 
-  def setup_top_players
-    @players = Player.all.sort_by { |player| player.total_kills }.reverse
-    @gold_player = @players[0]
-    @silver_player = @players[1]
-    @bronze_player = @players[2]
+  def setup_top_causes
+    @top_causes = Kill.select("cause_of_death, COUNT(*) AS count")
+                      .group(:cause_of_death)
+                      .order('count DESC')
+                      .limit(3)
+  end
+
+  def setup_top_killers
+    @top_killers = Player.left_joins(:kills)
+                         .group('players.id')
+                         .select('players.*, COUNT(kills.id) as total_kills')
+                         .order('total_kills DESC')
+                         .limit(3)
   end
 end
